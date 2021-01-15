394ba23d6c45fe2456e14cdf9fcf44bdf767328a
godong <scombridae78@gmail.com>
2021-01-15 16:32:17 +0900

    init

---
+++ ToDo/js/bg.js
@@ -0,0 +1,24 @@
+const body = document.querySelector('body');
+
+const IMG_NUMBER = 3;
+
+function paintImage(imgNumber) {
+    // load image and append to body
+    // add bgImage class for css
+    const img = new Image();
+    img.src = `images/${imgNumber}.jpg`;
+    img.classList.add("bgImage");
+    body.appendChild(img);
+}
+
+function genRandom() { // get random number between 0~IMG_NUMBER-1
+    const number = Math.floor(Math.random() * IMG_NUMBER) + 1;
+    return number;
+}
+
+function init() {
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+init();
